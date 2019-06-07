c347cba1dd1d0733b3bb83a486a9606b663faca0
roms13 <romain.kelifa@gmail.com>
2019-06-07 19:52:05 +0700

    :card_file_box: Add more data and modify structure

---
+++ lib/data/Category.dart
@@ -0,0 +1,5 @@
+enum Category {
+  kithain,
+  gallain,
+  thallain
+}
--- lib/data/Kith.dart
+++ lib/data/Kith.dart
@@ -1,11 +1,13 @@
 import 'dart:math';
+import './Category.dart';
 class LightKith {
   final String name;
+  final Category category;
   final String icon;
   final String image;
   final String description;
   final Point position;
-  LightKith(this.name, this.icon, this.image, this.description, {this.position: const Point(0.0, 0.0)});
+  LightKith(this.name, this.category, this.icon, this.image, this.description, {this.position: const Point(0.0, 0.0)});
 }
 class Kith extends LightKith {
   final String childing;
@@ -13,11 +15,12 @@ class Kith extends LightKith {
   final String grump;
   Kith(
       String name,
+      Category category,
       String icon,
       String image,
       String description,
       this.childing,
       this.wilder,
       this.grump,
-      {Point position}) : super(name, icon, image, description, position: position);
+      {Point position}) : super(name, category, icon, image, description, position: position);
 }
